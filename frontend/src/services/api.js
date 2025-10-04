741209717c396269fd1c60a30fdcf8f010c8b7d2
coldup-cpu <ggiinnggerr@gmail.com>
2025-10-04 14:10:44 +0530

    SKFood Customer Pages - Complete Implementation

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -24,4 +24,35 @@ export const adminAPI = {
   getOrderById: (id) => api.get(`/admin/orderwithId/${id}`),
 };
 
+// Add user API functions
+export const userAPI = {
+  // Menu
+  getLunchMenu: () => api.get('/userPanel/seeLunchMenu'),
+  getDinnerMenu: () => api.get('/userPanel/seeDinnerMenu'),
+  
+  // Orders
+  createOrder: (orderData) => api.post('/userPanel/orderPreparedThali', orderData),
+  getAllOrders: () => api.get('/userPanel/myAllOrders'),
+  getConfirmedOrders: () => api.get('/userPanel/confirmedOrders'),
+  getOrderById: (id) => api.get(`/userPanel/myOrderwithId/${id}`),
+};
+
+// Add auth API functions
+export const authAPI = {
+  signup: (userData) => api.post('/userAuth/signup', userData),
+  login: (credentials) => api.post('/userAuth/login', credentials),
+  logout: () => api.post('/userAuth/logout'),
+  getProfile: () => api.get('/userAuth/profile'),
+  googleAuth: () => window.location.href = '/api/userAuth/google',
+};
+
+// Add token to requests if available
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
 export default api;
